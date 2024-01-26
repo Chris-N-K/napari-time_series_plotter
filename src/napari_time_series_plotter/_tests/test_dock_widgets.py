52452a8254d6a92713ab5128a5d5cf33a948546b
Chris-N-K <chnauroth@web.de>
2024-01-26 19:39:51 +0100

    added tests for the reworked dock_widgets

--- src/napari_time_series_plotter/_tests/test_dock_widgets.py
+++ src/napari_time_series_plotter/_tests/test_dock_widgets.py
@@ -1,101 +1,62 @@
 """
-Napari-time_series_plotter dock_widget tests.
+Napari-time_series_plotter dock_widgets tests.
 """
-import numpy as np
-from napari.layers import Image
-from pytestqt import qtbot
-from qtpy import QtWidgets
-from qtpy.QtCore import QItemSelectionModel, Signal, QObject
+import napari
+import pytest
+from qtpy.QtCore import Qt
+from qtpy.QtGui import QStandardItemModel
 
-from ..dock_widgets import TSPExplorer, TSPInspector
-from ..widgets import LayerSelector, VoxelPlotter, OptionsManager
+from ..dock_widgets import (
+    TimeSeriesExplorer,
+    TimeSeriesTableView,
+)
 
 
-def test_TSPExplorer_init(make_napari_viewer):
+@pytest.fixture
+def explorer(make_napari_viewer):
     viewer = make_napari_viewer(show=False)
-    explorer = TSPExplorer(viewer)
+    yield TimeSeriesExplorer(viewer)
 
-    # test plotter
-    plotter = explorer.findChild(VoxelPlotter)
-    assert plotter
 
-    # test options
-    options = explorer.findChild(OptionsManager)
-    assert options
-
-    # test tabs
-    tabwidget = explorer.findChild(QtWidgets.QTabWidget)
-    assert tabwidget
-    assert all([tabwidget.tabText(ind) == gt for ind, gt in zip(range(2), ['Plotter', 'Options'])])
-    assert all([tabwidget.widget(ind) == gt for ind, gt in zip(range(2), [plotter, options])])
-
-    # test selector
-    selector = explorer.findChild(LayerSelector)
-    assert selector
-
-    # test datatable model
-    assert explorer.datatable
-
-    #TODO: add event tests
-
-def test_TSPExplorer_callbacks(make_napari_viewer):
+@pytest.fixture
+def tableview(make_napari_viewer):
     viewer = make_napari_viewer(show=False)
-    explorer = TSPExplorer(viewer)
-
-    # setup mock selector
-    class MockSelector:
-        def __init__(self) -> None:
-            self.res = [True, True, True]
-        
-        def update_model(self, value, etype):
-            # value tests; allowed -> Image, 3D to nD
-            is_image = isinstance(value, Image)
-            is_correct_dim = value.ndim >= 3
-            # etype tests; allowed -> inserted, removed, reordered
-            is_correct_type = etype in ['inserted', 'removed', 'reordered']
-
-            assert self.res == [is_image, is_correct_dim, is_correct_type]
-
-    explorer.selector = MockSelector()
-
-    viewer.add_image(np.random.randint(0, 100, (10, 10)), name='2D')
-    viewer.add_image(np.random.randint(0, 100, (10, 10, 10)), name='3D')
-    viewer.add_image(np.random.randint(0, 100, (10, 10, 10, 10)), name='4D')
-    viewer.layers.remove('3D')
+    yield TimeSeriesTableView(viewer)
 
 
-def test_TSPInspector(make_napari_viewer, qtbot: qtbot, monkeypatch):
+# TimeSeriesExplorer tests
+def test_TimeSeriesExplorer(explorer):
+    # Test TimeSeriesExplorer init
+    assert explorer
+    assert explorer._napari_viewer == napari.current_viewer()
+    assert explorer.layout()
+
+
+# TimeEsriesTableView tests
+def test_TimeSeriesTableView(tableview):
+    # Test TimeSeriesTableView init
+    assert tableview
+    assert isinstance(tableview.source_model, QStandardItemModel)
+    assert tableview.layout()
+
+
+def test_TimeSeriesTableView_connect_callbacks(
+    make_napari_viewer, mocker, qtbot
+):
+    # Test button clicked signal connections
     viewer = make_napari_viewer(show=False)
-    inspector = TSPInspector(viewer)
-    assert inspector.model
+    mocker_copyToClipboard = mocker.patch.object(
+        TimeSeriesTableView, "_copyToClipboard"
+    )
+    mocker_exportToCSV = mocker.patch.object(
+        TimeSeriesTableView, "_exportToCSV"
+    )
+    tableview = TimeSeriesTableView(viewer)
 
-    class MockModel(QObject):
-        called = Signal()
+    # Copy to clipboard btn
+    qtbot.mouseClick(tableview.btn_copy, Qt.MouseButton.LeftButton)
+    mocker_copyToClipboard.assert_called()
 
-        def __init__(self) -> None:
-            super().__init__()
-
-        def toClipboard(self, selection):
-            self.called.emit()
-        
-        def toCSV(self, path, selection):
-            self.called.emit()
-        
-        def update(self):
-            self.called.emit()
-
-    # monkeypatch the file dialog call in _exportToCSV
-    def mockreturn(a, b):
-        return '...', None
-    
-    monkeypatch.setattr(QtWidgets.QFileDialog, "getSaveFileName", mockreturn)
-
-    inspector.model = MockModel()
-    with qtbot.waitSignal(inspector.model.called, timeout=100):
-        inspector._toClipboard()
-    
-    with qtbot.waitSignal(inspector.model.called, timeout=100):
-        inspector._exportToCSV()
-
-    with qtbot.waitSignal(inspector.model.called, timeout=100):
-        inspector._updateData()
+    # Export to csv btn
+    qtbot.mouseClick(tableview.btn_export, Qt.MouseButton.LeftButton)
+    mocker_exportToCSV.assert_called()
